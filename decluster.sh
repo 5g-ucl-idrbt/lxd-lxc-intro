16f24f752f11ef397e8d280d9fed3fd0349e4eef
V Abhishek Raj <46273637+abhic137@users.noreply.github.com>
2023-07-31 10:04:41 +0530

    Add files via upload

---
+++ decluster.sh
@@ -0,0 +1,21 @@
+ls /var/snap/lxd/common/lxd/logs
+snap remove lxd
+snap remove lxc
+apt remove -y lxd
+apt autoremovesnap remove lxd
+remove lxc
+apt remove -y lxd
+apt autoremove
+zpool list
+apt purge lxd
+ls /var/snap/lxd/common/lxd/logs
+dpkg --purge --force-all lxd
+dpkg --reconfigure -a
+zpool destroy local
+apt-get remove --purge snapd
+sudo rm -rf /snap
+sudo rm -rf /var/snap
+rm -vf /etc/systemd/snap-.mount
+rm -vf /etc/systemd/snap-.service
+rm -vf /etc/systemd/snap-*.service
+ls /var/snap/lxd/common/lxd/logs
---
+++ recluster.sh
@@ -0,0 +1,11 @@
+ls /var/snap/lxd/common/lxd/logs
+apt install snap
+apt install snapd
+snap install lxd
+ls /var/snap/lxd/common/lxd/logs
+pause
+lxd init
+lxc version
+lxc info
+ls /var/snap/lxd/common/lxd/logs
+lxc list
